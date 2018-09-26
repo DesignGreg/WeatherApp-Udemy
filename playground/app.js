31756c01cdfb2293f480b5555af79d79ecc13e2c
Gregory <huyghegregory@yahoo.fr>
2018-09-26 22:28:30 +0200

    use API, install yargs to get any address, and encodeURI to put in into the http request

--- playground/app.js
+++ playground/app.js
@@ -1,8 +1,28 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+.options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather for',
+        string: true
+    }
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+console.log(argv);
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+st+philadelphia&key=AIzaSyB1O2jFzS3-W-3puT64RCyX39808TvKMyg',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress + '',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
