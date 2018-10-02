f45fdc4b0c92d961ecb6fb7ad4a06e0cd2a59305
Gregory <huyghegregory@yahoo.fr>
2018-10-02 22:57:18 +0200

    first step with Dark Sky weather API

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
-const yargs = require('yargs');
+//const yargs = require('yargs');
+//
+//const geocode = require('./geocode/geocode');
+//
+//const argv = yargs
+//    .options({
+//        a: {
+//            demand: true,
+//            alias: 'address',
+//            describe: 'Address to fetch weather for',
+//            string: true
+//        }
+//    })
+//    .help()
+//    .alias('help', 'h')
+//    .argv;
+//
+//console.log(argv);
+//
+//geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//    if (errorMessage) {
+//        console.log(errorMessage);
+//    } else {
+//        console.log(JSON.stringify(results, undefined, 2));
+//    }
+//});
 
-const geocode = require('./geocode/geocode');
+const request = require('request');
 
-const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: 'address',
-            describe: 'Address to fetch weather for',
-            string: true
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
-
-console.log(argv);
-
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if (errorMessage) {
-        console.log(errorMessage);
+request({
+    url: 'https://api.darksky.net/forecast//48.866667,2.333333',
+    json: true
+}, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+        console.log(body.currently.temperature);
     } else {
-        console.log(JSON.stringify(results, undefined, 2));
+        console.log('Unable to fetch weather.');
     }
 });
